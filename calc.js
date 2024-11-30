88cbcb354c902e7035e61d247c220246e028fe01
Marouane <184505120+marj-ayy@users.noreply.github.com>
2024-11-30 12:34:06 +0100

    Add operate func to js file

--- calc.js
+++ calc.js
@@ -13,3 +13,21 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+function operate(op, a, b){
+    switch(op){
+        case "+":
+            return add(a, b);
+        case "-":
+            return substract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+
