1a9e07e5e4d0a507bb97bdcc78fd879142d12abf
Marouane <184505120+marj-ayy@users.noreply.github.com>
2024-12-01 10:30:35 +0100

    Add calc logic and finish styling

--- calc.js
+++ calc.js
@@ -29,15 +29,85 @@ function operate(op, a, b){
 
 function populateDisplay(digit) {
     const display = document.querySelector(".display");
-    console.log(digit)
-    display.textContent = digit;
+    display.textContent += digit;
 }
 
-let firstNumber;
-let secondNumber;
-let operator;
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let switchToSecond = false;
+let errorClear = false;
 
-const digitButtons = document.querySelectorAll(".btn.digit");
-const arrayDigitButtons = Array.from(digitButtons);
-arrayDigitButtons.forEach(button => button.addEventListener("click", e => populateDisplay(e.target.textContent)));
+let counterOperator = 0;
+let counterEqual = 0;
+
+const buttons = document.querySelectorAll(".btn");
+const buttonsArray = Array.from(buttons);
+buttonsArray.forEach(button => button.addEventListener("mouseenter", (e) => {
+    e.target.style["opacity"] = 0.5;
+}));
+buttonsArray.forEach(button => button.addEventListener("mouseleave", (e) => {
+    e.target.style["opacity"] = 1;
+}));
+
+buttonsArray.forEach(button => button.addEventListener("click", (e) => {
+    const display = document.querySelector(".display");
+    const classArray = Array.from(e.target.classList);
+    if(errorClear)
+        display.textContent = "";
+        errorClear = false;
+    if (classArray.includes("digit")){
+        if (counterOperator === counterEqual)
+            firstNumber = firstNumber + e.target.textContent;
+        else
+            secondNumber = secondNumber + e.target.textContent;
+        if(switchToSecond){
+            display.textContent = "";
+            switchToSecond = false;
+        }
+        populateDisplay(e.target.textContent);
+    }
+    if(firstNumber){
+        if(classArray.includes("operator") && counterOperator === counterEqual){
+            ++counterOperator;
+            switchToSecond = true;
+            operator = e.target.textContent;
+        }
+    }
+    if(firstNumber && secondNumber && operator){
+        if(classArray.includes("equal")){
+            if(secondNumber === "0" && operator === "/"){
+                display.textContent = "";
+                display.textContent = "Math Error";
+                errorClear = true;
+                firstNumber = "";
+                secondNumber = "";
+                operator = "";
+                counterEqual = 0;
+                counterOperator = 0;
+            }
+            else{
+                ++counterEqual;
+                display.textContent = "";
+                let result = operate(operator, Number(firstNumber), Number(secondNumber));
+                if(result.toString().length > 10)
+                    result = Number.parseFloat(result).toExponential(5);
+                populateDisplay(result);
+                firstNumber = operate(operator, Number(firstNumber), Number(secondNumber)).toString(); 
+                secondNumber = ""; 
+            }   
+        }
+    }
+    if(classArray.includes("clear")){
+        firstNumber = "";
+        secondNumber = "";
+        operator = "";
+        counterEqual = 0;
+        counterOperator = 0;
+        display.textContent = "";
+    }   
+    console.log(`CounterOperator : ${counterOperator} // CounterEqual : ${counterEqual} // firstNumber : ${firstNumber} // secondNumber : ${secondNumber} // Operator : ${operator}`);
+}));
+
+
 
